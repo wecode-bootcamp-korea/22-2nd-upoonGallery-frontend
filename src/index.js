bdad5a4ee19434d18d5f7d84634d9f699d99ba96
Jongmin Lee <jongminlee8910@gmail.com>
2021-07-19 17:32:49 +0900

    초기셋팅완료

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 import './styles/reset.css';
 import './styles/variable.css';
 import './styles/common.css';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </>,
+  document.getElementById('root')
+);
