200cc619b7877186156b3c548164c815b2cbc44e
Jongmin Lee <jongminlee8910@gmail.com>
2021-07-20 17:27:44 +0900

    [Add] Login Page LayOut & Nav Bar LayOut
    [Fixed] 1차 피드백 반영 수정완료
    
    [Fixed] 2차 피드백 반영 수정완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,130 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router';
 
 const Nav = props => {
-  return <div>hi</div>;
+  const history = useHistory();
+
+  return (
+    <NavBox>
+      <NavContainer>
+        <Logo>
+          <i className="fas fa-paint-brush fa-3x"></i>
+          <LogoText>
+            <span>UPOON</span>
+            <span>GALLERY</span>
+          </LogoText>
+        </Logo>
+        <MenuContain>
+          <NavMenu>렌탈하기</NavMenu>
+          <NavMenu>이용후기</NavMenu>
+          <NavMenu>작가소개</NavMenu>
+        </MenuContain>
+        <MyPageContain>
+          <i className="far fa-image fa-2x"></i>
+          <i className="fas fa-search fa-2x"></i>
+          <i class="fas fa-shopping-cart fa-2x"></i>
+          <UserContain>
+            <button onClick={() => history.push('/login')}>로그인</button>
+            <button onClick={() => history.push('/signup')}>회원가입</button>
+          </UserContain>
+        </MyPageContain>
+      </NavContainer>
+    </NavBox>
+  );
 };
 
+const NavBox = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  background-color: white;
+  height: 60px;
+  box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2);
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: white;
+`;
+
+const Logo = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 90px;
+  border: none;
+  background-color: white;
+
+  .fas {
+    margin-right: 10px;
+    color: pink;
+  }
+`;
+const LogoText = styled.div`
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const MenuContain = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 150px;
+`;
+
+const NavMenu = styled.div`
+  padding-left: 70px;
+  font-size: 14px;
+`;
+
+const MyPageContain = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 220px;
+
+  .far {
+    margin-left: 20px;
+    margin-right: 10px;
+    color: pink;
+    font-size: 30px;
+  }
+
+  .fas {
+    margin-left: 20px;
+    margin-right: 10px;
+    color: pink;
+    font-size: 25px;
+  }
+`;
+
+const UserContain = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 10px;
+  margin-left: 10px;
+  height: 60px;
+  border-left: 1px solid #eee;
+
+  button {
+    border: none;
+    background-color: transparent;
+    margin-left: 20px;
+    font-size: 14px;
+    cursor: pointer;
+  }
+`;
 export default Nav;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,132 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Login = props => {
-  return <div>hi</div>;
+  return (
+    <LoginPage>
+      <LoginContain>
+        <h1>로그인</h1>
+        <BtnContainer>
+          <KaKaoBtn>
+            <i className="fas fa-comment"></i>
+            <BtnText>카카오로 로그인</BtnText>
+          </KaKaoBtn>
+          <FaceBookBtn>
+            <i className="fab fa-facebook-f"></i>
+            <BtnText>페이스북으로 로그인</BtnText>
+          </FaceBookBtn>
+          <GoogleBtn>
+            <i className="fab fa-google-plus-g"></i>
+            <BtnText>구글로 로그인</BtnText>
+          </GoogleBtn>
+        </BtnContainer>
+        <GoToSignUp>
+          아직 회원이 아니시라면?
+          <Link className="linkToSignUp" to="/signup">
+            회원가입
+          </Link>
+        </GoToSignUp>
+      </LoginContain>
+    </LoginPage>
+  );
 };
+
+const LoginPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 140px 0;
+`;
+
+const LoginContain = styled.div`
+  text-align: center;
+  align-items: center;
+  padding: 20px 50px;
+  margin: 0 auto;
+  border: 1px solid #dfdfdf;
+
+  h1 {
+    font-size: 20px;
+    font-weight: bold;
+    letter-spacing: 0.5px;
+    margin: 30px 0;
+  }
+`;
+
+const BtnContainer = styled.div`
+  align-items: center;
+  margin: 0 auto 20px;
+`;
+
+const BtnText = styled.span`
+  text-align: center;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const LoginBtn = styled.button`
+  display: block;
+  position: relative;
+  padding: 0;
+  margin: 50px 20px;
+  width: 300px;
+  height: 45px;
+  line-height: 44px;
+  align-items: center;
+  color: #783c00;
+  background-color: #ffeb00;
+  border: 1px solid transparent;
+  border-radius: 3px;
+  text-align: center;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const KaKaoBtn = styled(LoginBtn)`
+  color: #783c00;
+
+  .fas {
+    position: absolute;
+    top: 15px;
+    left: 5px;
+  }
+`;
+
+const FaceBookBtn = styled(LoginBtn)`
+  color: #ffffff;
+  background-color: #5577ba;
+
+  .fab {
+    position: absolute;
+    top: 15px;
+    left: 5px;
+  }
+`;
+
+const GoogleBtn = styled(LoginBtn)`
+  color: #ffffff;
+  background-color: #f04031;
+
+  .fab {
+    position: absolute;
+    top: 15px;
+    left: 5px;
+  }
+`;
+
+const GoToSignUp = styled.div`
+  margin-bottom: 40px;
+  font-size: 12px;
+  color: #989898;
+
+  .linkToSignUp {
+    margin-left: 10px;
+    color: #989898;
+    font-weight: 500;
+  }
+`;
 
 export default Login;
