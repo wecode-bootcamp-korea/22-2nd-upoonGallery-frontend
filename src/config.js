196cc662a8706031603bd5c6b98dd4402febc573
lee5992 <dlwogus432@gmail.com>
2021-07-22 20:34:07 +0900

    [Fix] : 필터링, 정렬, 페이지네이션 및 상세페이지 이동기능 구현

--- src/config.js
+++ src/config.js
@@ -1,2 +1,7 @@
 export const API = 'http://10.58.0.238:8000';
 export const LOGIN_API = `${API}/users/signin/kakao`;
+
+const JAPI = 'http://10.58.2.86:8000';
+export const J_API = {
+  DISCOVER: `${JAPI}/arts`,
+};
